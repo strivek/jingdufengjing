e30f89b871eee604c6db7c9b5ba012abfcd2b254
jingxijun <jingxijun@yeah.net>
2014-10-22 14:06:28 +0800

    add bigman arr scroll

--- web/javascript/bigman/main.js
+++ web/javascript/bigman/main.js
@@ -9,6 +9,39 @@ require(['jquery'], function ($) {
                 box.hide().eq(index).fadeIn(500);
             })
         });
+        if(len > 4){
+            var list = $('.m-bigman .btn');
+            $('<span class="up"></span><span class="down"></span>').insertAfter(list);
+            var up = $('.up');
+            var down = $('.down');
+            up.hide();
+            up.on('click', function () {
+                down.show();
+                if (list.is(':animated')) {
+                    return;
+                }
+                if (parseInt(list.css('top')) > -2){
+                    up.hide();
+                }
+                list.animate({'top':'+=100px'},300,function(){
+                    if (parseInt(list.css('top')) > -2){
+                        up.hide();
+                    }
+                });
+            });
+
+            down.on('click', function () {
+                up.show();
+                if (list.is(':animated')) {
+                    return;
+                }
+                list.animate({'top':'-=100px'},300,function(){
+                    if (parseInt(list.css('top')) < -(len-4)*100){
+                        down.hide();
+                    }
+                });
+            });
+        }
     })
 });
 
