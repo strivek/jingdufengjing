4b87c41dea72a76487a7d95fe6e94827abfc1f74
chengyuan <chengyuan9826@qq.com>
2014-10-23 12:36:52 +0800

    首页再次更改

--- web/javascript/Carousel/main.js
+++ web/javascript/Carousel/main.js
@@ -1,4 +1,12 @@
 require(['jquery','Carousel/Carousel'], function ($) {
+    var $num_pList=$(".peopleList .items li").length;
+    if($num_pList==1)
+    {$(".peopleList").addClass("clist_1");}
+    else if($num_pList==2){
+        $(".peopleList").addClass("clist");
+    }
+    else {
+        $(".peopleList").addClass("clist");
         $('.clist').flexslider({
             animation: "slide",
             animationLoop: true,
@@ -8,4 +16,7 @@ require(['jquery','Carousel/Carousel'], function ($) {
                 $('body').removeClass('loading');
             }
         });
+    }
+
+
 })
