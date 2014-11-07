c1b549407c9828dd465c9624c9049dac6f73a4ae
jingxijun <jingxijun@yeah.net>
2014-11-07 19:12:41 +0800

    1107

--- web/javascript/bigman/main.js
+++ web/javascript/bigman/main.js
@@ -6,8 +6,29 @@ require(['jquery'], function ($) {
             box.eq(0).css('visibility','visible');
             btn.each(function (index) {
                 $(this).on('click', function () {
+                    var $this = $(this);
+                    var who = $this.data("who");
                     btn.removeClass('on').eq(index).addClass('on');
                     box.hide().eq(index).css('visibility','visible').fadeIn(500);
+                    /*var result = $.ajax({
+                        type: "GET",
+                        url: "json/data.json",
+                        data: { who: who }
+                    });
+                    result.done(function(data){
+                        var dataNews = '';
+                        $.each(data.news, function (idx, news) {
+                            dataNews += '<a class="link" target="_blank" href="' + news.link + '"><span>查看详情</span>' + news.title + '</a>';
+                        });
+                        //console.log(dataNews);
+                        $('.j-newsBox').html(dataNews);
+                        var dataImages = '';
+                        $.each(data.images, function (idx, images) {
+                            dataImages += '<a class="item" target="_blank" href="' + images.link + '"><img src="' + images.src + '" alt="img"/><div class="text-bg"></div><div class="text">' + images.text + '</div></a>';
+                        });
+                        //console.log(dataImages);
+                        $('.j-imagesBox').html(dataImages);
+                    })*/
                 })
             });
             if (len == 1){
