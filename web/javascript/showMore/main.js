1cfe641de80c567ddc788e27dcbf36958c36dc53
chengyuan <chengyuan9826@qq.com>
2014-11-21 21:43:33 +0800

    坚决不改版

--- web/javascript/showMore/main.js
+++ web/javascript/showMore/main.js
@@ -1,8 +1,23 @@
 require(['jquery'], function ($) {
 var $btn_dow=$(".up_show");
 var $img_box=$(".g-bfcontent  .img_box");
+var $item=$img_box.find(".item");
 var $btn_dow_li=$(".up_show_list");
-var $list_box=$(".m-linklist-1 .list_box")
+var $list_box=$(".m-linklist .list_box");
+var $link=$list_box.find(".link");
+
+    $btn_dow.css("display","none");
+    $btn_dow_li.css("display","none");
+
+    if($item.length<=4){
+        $btn_dow.css("display","none");
+     }
+    else{ $btn_dow.css("display","block");}
+
+    if($link.length<=5){
+        $btn_dow_li.css("display","none");
+    }
+    else{ $btn_dow_li.css("display","block");}
 
 $btn_dow.on("click",function(){
     if($img_box.height()==366){
